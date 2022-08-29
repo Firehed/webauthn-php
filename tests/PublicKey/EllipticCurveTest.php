e69e32d64cfdb97a1092e8fd2258457bd283537d
Eric Stern <eric@ericstern.com>
2022-08-29 15:09:17 -0700

    Add tests for pubkey formatting (#12)
    This also does an inconsequential tweak around line endings, which doesn't seem to impact any of the internal handling.

---
+++ tests/PublicKey/EllipticCurveTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Firehed\WebAuthn\PublicKey;
+
+use Firehed\WebAuthn\BinaryString;
+
+/**
+ * @covers Firehed\WebAuthn\PublicKey\EllipticCurve
+ */
+class EllipticCurveTest extends \PHPUnit\Framework\TestCase
+{
+    public function testFormatHandling(): void
+    {
+        // Generated a keypair with openssl:
+        //   openssl ecparam -name secp256r1 -genkey -noout -out pk.pem
+        //
+        // Get key info:
+        //   openssl ec -in pk.pem -text -noout
+        //
+        // Manually copied the output in the `pub:` section, removing the
+        // leading `04:` (which is a format indicator for uncompressed) and set
+        // into $x and $y below
+        //
+        // Extracted public key:
+        //    openssl ec -in pk.pem -pubout pub.pem
+        // and copied into $opensslPubKey
+
+        $x = BinaryString::fromHex('0f06777d44842cce4a2e7d00587b3fc892a7da7cf1704a8dd1ffb7e5334721a8');
+        $y = BinaryString::fromHex('3f017188437532409d6bbc86b68d56214a720bf8c183f844c576f4e2003ba976');
+
+        $pk = new EllipticCurve($x, $y);
+        self::assertSame($x->unwrap(), $pk->getXCoordinate(), 'X-coordinate changed');
+        self::assertSame($y->unwrap(), $pk->getYCoordinate(), 'Y-coordinate changed');
+
+        $opensslPubKey = <<<PEM
+        -----BEGIN PUBLIC KEY-----
+        MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEDwZ3fUSELM5KLn0AWHs/yJKn2nzx
+        cEqN0f+35TNHIag/AXGIQ3UyQJ1rvIa2jVYhSnIL+MGD+ETFdvTiADupdg==
+        -----END PUBLIC KEY-----
+        PEM;
+
+        self::assertSame($opensslPubKey, $pk->getPemFormatted(), 'pubkey format changed');
+    }
+}
