9f062c7abdb7b4d2bb7e0bad1b44c25606a87277
Eric Stern <eric@ericstern.com>
2023-11-15 13:32:46 -0800

    formats on, update match

--- src/Attestations/Format.php
+++ src/Attestations/Format.php
@@ -9,13 +9,11 @@ namespace Firehed\WebAuthn\Attestations;
  */
 enum Format: string
 {
+    case AndroidKey = 'android-key';
+    case AndroidSafetyNet = 'android-safetynet';
+    case Apple = 'apple';
     case None = 'none';
+    case Packed = 'packed';
+    case TPM = 'tpm';
     case U2F = 'fido-u2f';
-    // These are defined in the spec but not yet supported by the library, and
-    // as such are left disabled for clarity.
-    // case AndroidKey = 'android-key';
-    // case AndroidSafetyNet = 'android-safetynet';
-    // case Apple = 'apple';
-    // case Packed = 'packed';
-    // case TPM = 'tpm';
 }
