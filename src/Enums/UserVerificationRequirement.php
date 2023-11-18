a08f2442897d3343980b6959894811fe505f0d64
Eric Stern <eric@ericstern.com>
2023-11-18 11:41:09 -0800

    Move UVR

--- src/Enums/UserVerificationRequirement.php
+++ src/Enums/UserVerificationRequirement.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace Firehed\WebAuthn\Enums;
 
+/**
+ * @link https://w3c.github.io/webauthn/#enum-userVerificationRequirement
+ *
+ * @api
+ */
 enum UserVerificationRequirement: string
 {
     case Discouraged = 'discouraged';
