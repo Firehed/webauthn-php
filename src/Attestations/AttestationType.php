50d1be1b7ac19794657c3f28b5921ee574d82479
Eric Stern <eric@ericstern.com>
2023-12-06 18:21:51 -0800

    define the uncertainty path

--- src/Attestations/AttestationType.php
+++ src/Attestations/AttestationType.php
@@ -7,8 +7,8 @@ namespace Firehed\WebAuthn\Attestations;
 /**
  * @internal
  *
- * @see 6.5.3
- * @link https://www.w3.org/TR/webauthn-2/#sctn-attestation-types
+ * @see 6.5.4
+ * @link https://www.w3.org/TR/webauthn-3/#sctn-attestation-types
  */
 enum AttestationType
 {
@@ -17,4 +17,9 @@ enum AttestationType
     case AttestationCA; // = 'AttCA';
     case AnonymizationCA; // = 'AnonCA';
     case None; // = 'None';
+
+    // "uncertainty" isn't really clearly defined in the spec, but it's being
+    // used here for "the library probably needs to do more work or be given
+    // more data"
+    case Uncertain;
 }
