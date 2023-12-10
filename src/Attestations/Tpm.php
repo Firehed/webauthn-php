663fc20538aace5892d451903e2b298b26b82940
Eric Stern <eric@ericstern.com>
2023-12-10 11:30:01 -0800

    Basic structure

--- src/Attestations/Tpm.php
+++ src/Attestations/Tpm.php
@@ -4,11 +4,22 @@ declare(strict_types=1);
 
 namespace Firehed\WebAuthn\Attestations;
 
-class Tpm
+use Exception;
+use Firehed\CBOR\Decoder;
+use Firehed\WebAuthn\AuthenticatorData;
+use Firehed\WebAuthn\BinaryString;
+use Firehed\WebAuthn\Certificate;
+use Firehed\WebAuthn\PublicKey\EllipticCurve;
+use OpenSSLCertificate;
+
+class Tpm implements AttestationStatementInterface
 {
     public function __construct(
         private array $data,
     ) {
     }
 
+    public function verify(AuthenticatorData $data, BinaryString $clientDataHash): VerificationResult
+    {
+    }
 }
