8eb8d3606964d9d61c9b89da592d1304e93f0a6d
Eric Stern <eric@ericstern.com>
2024-03-09 11:06:23 -0800

    first-pass move key parsing into format-specific impl

--- src/COSEKey.php
+++ src/COSEKey.php
@@ -23,25 +23,26 @@ use Firehed\CBOR\Decoder;
  * @link https://www.rfc-editor.org/rfc/rfc8152.html
  *
  * @see RFC 8230 (RSA key support - not yet implemented)
+ *
+ * @see RFC 9052
+ * @link https://www.rfc-editor.org/rfc/rfc9052
  */
 class COSEKey
 {
     // Data structure indexes
-    // @see section 7.1
-    private const INDEX_KEY_TYPE = 1;
-    private const INDEX_ALGORITHM = 3;
-    // 13.1.1-13.2
-    private const INDEX_CURVE = -1; // ECC, OKP
-    private const INDEX_X_COORDINATE = -2; // ECC, OKP
-    private const INDEX_Y_COORDINATE = -3; // ECC
-    private const INDEX_PRIVATE_KEY = -4; // ECC, OKP @phpstan-ignore-line
-    // index_key_value = -1 (same as index_curve, for Symmetric)
+    // @see RFC 9052 §7.1
+    public const INDEX_KEY_TYPE = 1;
+    public const INDEX_KEY_ID = 2;
+    public const INDEX_ALGORITHM = 3;
+    public const INDEX_KEY_OPS = 4;
+    public const INDEX_BASE_IV = 5;
 
-    private COSE\KeyType $keyType;
-    public readonly COSE\Algorithm $algorithm;
-    private COSE\Curve $curve;
-    private BinaryString $x;
-    private BinaryString $y;
+    private PublicKey\PublicKeyInterface $publicKey;
+    // private COSE\KeyType $keyType;
+    // public readonly COSE\Algorithm $algorithm;
+    // private COSE\Curve $curve;
+    // private BinaryString $x;
+    // private BinaryString $y;
     // d ~ private key
 
     public function __construct(public readonly BinaryString $cbor)
@@ -55,29 +56,20 @@ class COSEKey
             throw new DomainException('Only EC2 keys supported');
         }
 
-        $algorithm = COSE\Algorithm::tryFrom($decodedCbor[self::INDEX_ALGORITHM]);
-        if ($algorithm !== COSE\Algorithm::EcdsaSha256) {
-            throw new DomainException('Only ES256 supported');
-        }
+        $this->publicKey = match ($keyType) {
+            COSE\KeyType::EllipticCurve => PublicKey\EllipticCurve::fromDecodedCbor($decodedCbor),
+        };
 
-        $curve = COSE\Curve::tryFrom($decodedCbor[self::INDEX_CURVE]);
-        if ($curve !== COSE\Curve::P256) {
-            throw new DomainException('Only curve P-256 (secp256r1) supported');
-        }
 
-        $this->keyType = $keyType;
-        $this->algorithm = $algorithm;
-        $this->curve = $curve;
 
-        if (strlen($decodedCbor[self::INDEX_X_COORDINATE]) !== 32) {
-            throw new DomainException('X coordinate not 32 bytes');
-        }
-        $this->x = new BinaryString($decodedCbor[self::INDEX_X_COORDINATE]);
+//         $curve = COSE\Curve::tryFrom($decodedCbor[self::INDEX_CURVE]);
+//         if ($curve !== COSE\Curve::P256) {
+//             throw new DomainException('Only curve P-256 (secp256r1) supported');
+//         }
 
-        if (strlen($decodedCbor[self::INDEX_Y_COORDINATE]) !== 32) {
-            throw new DomainException('X coordinate not 32 bytes');
-        }
-        $this->y = new BinaryString($decodedCbor[self::INDEX_Y_COORDINATE]);
+        // $this->keyType = $keyType;
+        // $this->algorithm = $algorithm;
+        // $this->curve = $curve;
 
         // d = cbor[INDEX_PRIVATE_KEY]
 
@@ -90,6 +82,7 @@ class COSEKey
      */
     public function getPublicKey(): PublicKey\PublicKeyInterface
     {
+        return $this->publicKey;
         // These are valid; the internal formats are brittle right now.
         assert($this->keyType === COSE\KeyType::EllipticCurve);
         assert($this->curve === COSE\Curve::P256);
--- src/PublicKey/EllipticCurve.php
+++ src/PublicKey/EllipticCurve.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Firehed\WebAuthn\PublicKey;
 
+use DomainException;
 use Firehed\WebAuthn\BinaryString;
+use Firehed\WebAuthn\COSE;
+use Firehed\WebAuthn\COSEKey;
 use UnexpectedValueException;
 
 /**
@@ -20,14 +23,66 @@ use UnexpectedValueException;
  */
 class EllipticCurve implements PublicKeyInterface
 {
-    public function __construct(private BinaryString $x, private BinaryString $y)
-    {
+    // CBOR decoding: RFC 9053 §7.1.1
+    private const INDEX_CURVE = -1; // ECC, OKP
+    private const INDEX_X_COORDINATE = -2; // ECC, OKP
+    private const INDEX_Y_COORDINATE = -3; // ECC
+    private const INDEX_PRIVATE_KEY = -4; // ECC, OKP @phpstan-ignore-line
+    // index_key_value = -1 (same as index_curve, for Symmetric)
+
+
+    private function __construct(
+        private COSE\Curve $curve,
+        private BinaryString $x,
+        private BinaryString $y,
+    ) {
         if ($x->getLength() !== 32) {
             throw new UnexpectedValueException('X-coordinate not 32 bytes');
         }
         if ($y->getLength() !== 32) {
             throw new UnexpectedValueException('Y-coordinate not 32 bytes');
         }
+    }
+
+    /**
+     * @param mixed[] $decoded
+     */
+    public static function fromDecodedCbor(array $decoded): EllipticCurve
+    {
+        // Checked upstream, but re-verify
+        assert(array_key_exists(COSEKey::INDEX_KEY_TYPE, $decoded));
+        $type = COSE\KeyType::from($decoded[COSEKey::INDEX_KEY_TYPE]);
+        assert($type === COSE\KeyType::EllipticCurve);
+
+
+        assert(array_key_exists(COSEKey::INDEX_ALGORITHM, $decoded));
+        $algorithm = COSE\Algorithm::from($decoded[COSEKey::INDEX_ALGORITHM]);
+        // TODO: support other algorithms
+        if ($algorithm !== COSE\Algorithm::EcdsaSha256) {
+            throw new DomainException('Only ES256 is supported');
+        }
+
+        $curve = COSE\Curve::from($decoded[self::INDEX_CURVE]);
+        // WebAuthn §5.8.5 - cross-reference curve to algorithm
+        assert($curve === COSE\Curve::P256);
+
+        if (strlen($decoded[self::INDEX_X_COORDINATE]) !== 32) {
+            throw new DomainException('X coordinate not 32 bytes');
+        }
+        $x = new BinaryString($decoded[self::INDEX_X_COORDINATE]);
+
+        if (strlen($decoded[self::INDEX_Y_COORDINATE]) !== 32) {
+            throw new DomainException('X coordinate not 32 bytes');
+        }
+        $y = new BinaryString($decoded[self::INDEX_Y_COORDINATE]);
+
+        // private key should not be present; ignoring it
+
+        return new EllipticCurve(
+            curve: $curve,
+            x: $x,
+            y: $y,
+        );
     }
 
     /**
@@ -52,7 +107,11 @@ class EllipticCurve implements PublicKeyInterface
     // public key component
     public function getPemFormatted(): string
     {
+        if ($this->curve !== COSE\Curve::P256) {
+            throw new DomainException('Only P256 curves can be PEM-formatted so far');
+        }
         // Described in RFC 5480
+        // §2.1.1.1
         // Just use an OID calculator to figure out *that* encoding
         $der = hex2bin(
             '3059' // SEQUENCE, length 89
